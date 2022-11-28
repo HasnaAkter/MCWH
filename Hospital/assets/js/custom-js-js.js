cbff600f8d30f131760f342cf2a788c107c82238
HasnaAkter <hasnaakter.cse@gmail.com>
2022-11-28 10:46:49 +0600

    first commit

---
+++ Hospital/assets/js/custom-js-js.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        items:1,
+        autoplay: true,
+        autoPlaySpeed: 5000,
+        dots: false,
+        nav: true,
+    })
+
+});
